0d5fe89f32c9ca86457b001ee1f267b7fe0ae079
ywCN <wangyouhao90@gmail.com>
2017-09-07 11:54:10 -0400

    bugged for index out of range

--- OpenAndPrint.py
+++ OpenAndPrint.py
@@ -4,19 +4,31 @@ class OpenAndPrint:
         while True:
             file_name = input('Enter the file name: ')
             try:
-                opened_file = open(file_name)
+                opened_file = open(file_name)  # use with?
                 break
-            except: # add exception name later
+            except FileNotFoundError:
                 print('File', file_name, 'cannot be opened. Please enter again.')
                 continue
         return opened_file
 
     def print_lines(self):
+        match = ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC",
+                 "FAMS", "FAM", "MARR", "HUSB", "WIFE", "CHIL",
+                 "DIV", "DATE", "HEAD", "TRLR", "NOTE"]
         opened = self.open_file()
 
         for line in opened:
-
             print("-->", line)
+
+            words = line.split(" ")
+            len = words.__sizeof__()
+            if words[1] in match:
+                print(words[0], "|", words[1], "|Y|", words[2])
+            elif words[-1] in match:
+                print(words[0], "|", words[-1], "|Y|", words[1])
+            else:
+                print(words[0], "|", words[1], "|N|", words[2])
+
 
 
 def main():
