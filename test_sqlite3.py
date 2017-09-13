b6bdd21a61e786a25a169fee4e7d3db4e426caa3
ywCN <wangyouhao90@gmail.com>
2017-09-13 10:19:32 -0400

    finished file

--- test_sqlite3.py
+++ test_sqlite3.py
@@ -5,3 +5,12 @@ c = conn.cursor()
 
 def create_table():
     c.execute("CREATE TABLE IF NOT EXISTS stuffToPlot(unix REAL, datestamp TEXT, keyword TEXT, value REAL)")
+
+def data_entry():
+    c.execute("INSET INTO stuffToPlot VALUES(14512354, '2017-07-05', 'Python', 5)")
+    conn.commit()
+    c.close()  # close cursor and connection
+    conn.close()
+
+create_table()
+data_entry()
