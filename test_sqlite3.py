775dadcbcf7538181435ba0f8b352bb41ddb8bc7
ywCN <wangyouhao90@gmail.com>
2017-09-13 11:56:54 -0400

    added a function to display graph

--- test_sqlite3.py
+++ test_sqlite3.py
@@ -2,41 +2,83 @@ import sqlite3
 import time
 import datetime
 import random
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from dateutil import parser
+from matplotlib import style
+style.use('fivethirtyeight')
 
-conn = sqlite3.connect("tutorial.db")
+
+conn = sqlite3.connect('tutorial.db')
 c = conn.cursor()
+
 
 def create_table():
     c.execute("CREATE TABLE IF NOT EXISTS stuffToPlot(unix REAL, datestamp TEXT, keyword TEXT, value REAL)")
 
+
 def data_entry():
-    c.execute("INSERT INTO stuffToPlot VALUES(14512354, '2017-07-02', 'Python', 5)")
+    c.execute("INSERT INTO stuffToPlot VALUES(1452549219,'2016-01-11 13:53:39','Python',6)")
+
     conn.commit()
-    c.close()  # close cursor and connection
+    c.close()
     conn.close()
 
+
 def dynamic_data_entry():
-    unix = time.time()  # will use a lot of space in DB
-    date = str(datetime.datetime.fromtimestamp(unix).strftime("%Y-%m-%d %H:%M:%S"))
-    keyword = "Python"
+    unix = int(time.time())
+    date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
+    keyword = 'Python'
     value = random.randrange(0, 10)
+
     c.execute("INSERT INTO stuffToPlot (unix, datestamp, keyword, value) VALUES (?, ?, ?, ?)",
               (unix, date, keyword, value))
+
     conn.commit()
+    time.sleep(1)
+
 
 def read_from_db():
-    c.execute("SELECT keyword, unix, value, datestamp FROM stuffToPlot WHERE unix > 14526187")
-    # keyword, unix, value, datestamp changes the output order of rows
-    # data = c.fetchall()
-    # print(data)
-    for row in c.fetchall():
-        print(row)  # row[0] to display only 1st col
+    c.execute('SELECT * FROM stuffToPlot')
+    data = c.fetchall()
+    print(data)
+    for row in data:
+        print(row)
 
-# create_table()
-# #data_entry()
-# for i in range(10):
-#     dynamic_data_entry()
-#     time.sleep(1)
-read_from_db()
-c.close()
+    c.execute('SELECT * FROM stuffToPlot WHERE value = 3')
+    data = c.fetchall()
+    print(data)
+    for row in data:
+        print(row)
+
+    c.execute('SELECT * FROM stuffToPlot WHERE unix > 1452554972')
+    data = c.fetchall()
+    print(data)
+    for row in data:
+        print(row)
+
+    c.execute('SELECT value, datestamp FROM stuffToPlot WHERE unix > 1452554972')
+    data = c.fetchall()
+    print(data)
+    for row in data:
+        print(row[0])
+
+
+def graph_data():
+    c.execute('SELECT datestamp, value FROM stuffToPlot')
+    data = c.fetchall()
+
+    dates = []
+    values = []
+
+    for row in data:
+        dates.append(parser.parse(row[0]))
+        values.append(row[1])
+
+    plt.plot_date(dates, values, '-')
+    plt.show()
+
+
+graph_data()
+c.close
 conn.close()
