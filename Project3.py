d91ac1fd62196f20c3ea0b71ad5a760228842efc
ywCN <wangyouhao90@gmail.com>
2017-09-16 20:53:49 -0400

    refactored 80%

--- Project3.py
+++ Project3.py
@@ -27,75 +27,51 @@ class OpenSavePrint:
         conn = sqlite3.connect('project.db')
         c = conn.cursor()
 
-        # indi_non_date_keys = ["NAME", "SEX", "FAMC", "FAMS"]  # 1
-        # indi_date_keys = ["BIRT", "DEAT"]  # 1
-        # fam_non_date_keys = ["HUSB", "WIFE", "CHIL"]  # 1
-        # fam_date_keys = ["MARR", "DIV"]  # 1
-        # individuals = {}
-        # {"indi_id": {"NAME": "", "SEX": "", "BIRT": "", "DEAT": "", "FAMC": "", "FAMS": ""}}
-        # families = {}
-        # {"fam_id": {"MARR": "", "DIV": "", "HUSB": "", "WIFE": ""}}
         date_tags = ["BIRT", "DEAT", "MARR", "DIV"]
         lines = self.open_file()
         date_name_cache = indi_id = fam_id = ""
         indi_tab = {"INDI": "", "NAME": "", "SEX": "", "FAMC": "", "FAMS": "", "BIRT": "", "DEAT": ""}
-        # INDI = NAME = SEX = FAMC = FAMS = BIRT = DEAT = ""
-        FAM = MARR = HUSB = WIFE = CHIL = DIV = ""
+        fam_tab = {"FAM": "", "MARR": "", "HUSB": "", "WIFE": "", "CHIL": "", "DIV": ""}
 
         for line in lines:
             words = line.strip().split()
             if words[0] == "0":
                 if words[-1] == "INDI":
                     if indi_id != "":
-                        c.execute("INSERT INTO indi (NAME, SEX, BIRT, DEAT, FAMC, FAMS) VALUES (?, ?, ?, ?, ?, ?)",
+                        c.execute("INSERT INTO indi (INDI, NAME, SEX, BIRT, DEAT, FAMC, FAMS) VALUES (?, ?, ?, ?, ?, ?)",
                                   (indi_tab["INDI"], indi_tab["NAME"], indi_tab["SEX"], indi_tab["FAMC"], indi_tab["FAMS"], indi_tab["BIRT"], indi_tab["DEAT"]))
                         conn.commit()
-                        INDI = NAME = SEX = FAMC = FAMS = BIRT = DEAT = ""
+
+                        for key in indi_tab.keys():
+                            indi_tab[key] = ""
                     else:
-                        INDI = words[1]
+                        indi_tab[words[-1]] = words[1]
                 if words[-1] == "FAM":
                     if indi_id != "":
-                        c.execute("INSERT INTO fam (MARR, HUSB, WIFE, CHIL, DIV) VALUES (?, ?, ?, ?, ?)",
-                            (MARR, HUSB, WIFE, CHIL, DIV))
+                        c.execute("INSERT INTO fam (FAM, MARR, HUSB, WIFE, CHIL, DIV) VALUES (?, ?, ?, ?, ?)",
+                            (fam_tab["FAM"], fam_tab["MARR"], fam_tab["HUSB"], fam_tab["WIFE"], fam_tab["CHIL"], fam_tab["DIV"]))
                         conn.commit()
-                        FAM = MARR = HUSB = WIFE = CHIL = DIV = ""
+
+                        for key in fam_tab.keys():
+                            fam_tab[key] = ""
                     else:
-                        FAM = words[1]
+                        fam_tab[words[-1]] = words[1]
+
             elif words[0] == "1":
-                # if words[1] in indi_tab:
-                #     NAME = " ".join(words[2:])
-                # elif words[1] == "SEX":
-                #     SEX = " ".join(words[2:])
-                # elif words[1] == "FAMC":
-                #     FAMC = " ".join(words[2:])
-                # elif words[1] == "FAMS":
-                #     FAMS = " ".join(words[2:])
-                # elif words[1] == "HUSB":
-                #     HUSB = " ".join(words[2:])
-                # elif words[1] == "WIFE":
-                #     WIFE = " ".join(words[2:])
-                # elif words[1] == "CHIL":
-                #     CHIL = " ".join(words[2:])
                 if words[1] in date_tags:
                     date_name_cache = words[1]  # cache it for "DATE"
                 elif words[1] in indi_tab:
                     indi_tab[words[1]] = " ".join(words[2:])
                 elif words[1] in fam_tab:
                     fam_tab[words[1]] = " ".join(words[2:])
-
                 else:
                     pass
 
-                date_tags.index()
             elif words[0] == "2" and words[0] == "DATE":
-                if date_name_cache == "BIRT":
-                    BIRT = " ".join(words[2:])
-                elif date_name_cache == "DEAT":
-                    DEAT = " ".join(words[2:])
-                elif date_name_cache == "MARR":
-                    MARR = " ".join(words[2:])
-                elif date_name_cache == "DIV":
-                    DIV = " ".join(words[2:])
+                if date_name_cache in indi_tab:
+                    indi_tab[date_name_cache] = " ".join(words[2:])
+                elif date_name_cache in fam_tab:
+                    fam_tab[date_name_cache] = " ".join(words[2:])
                 else:
                     print("Something is wrong with the date_name_cache!")
         c.close()
