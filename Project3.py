b885154ffb4bc16ab9e3c79a7d8a0741ce14abe9
ywCN <wangyouhao90@gmail.com>
2017-09-17 00:41:01 -0400

    fixed the bug which is missing last entries for each table. it should works perfectly!

--- Project3.py
+++ Project3.py
@@ -23,7 +23,23 @@ class Project3:
         c.execute(
             "CREATE TABLE IF NOT EXISTS fam(FAM TEXT, MARR TEXT, DIV TEXT, HUSB TEXT, WIFE TEXT, CHIL TEXT)")
 
-    def parse_lines(self, c, conn):
+    def insert_entry(self, table, c, conn):
+        data = list(table.values())
+
+        if len(data) == 7:
+            c.execute("INSERT INTO indi (INDI, NAME, SEX, BIRT, DEAT, FAMC, FAMS) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                (data[0], data[1], data[2], data[3], data[4], data[5], data[6]))
+
+        if len(data) == 6:
+            if isinstance(data[5], list):
+                c.execute("INSERT INTO fam (FAM, MARR, DIV, HUSB, WIFE, CHIL) VALUES (?, ?, ?, ?, ?, ?)",
+                          (data[0], data[1], data[2], data[3], data[4], ' '.join(data[5])))
+            else:
+                c.execute("INSERT INTO fam (FAM, MARR, DIV, HUSB, WIFE, CHIL) VALUES (?, ?, ?, ?, ?, ?)",
+                          (data[0], data[1], data[2], data[3], data[4], data[5]))
+        conn.commit()
+
+    def populate_table(self, c, conn):
 
         indi_tab = {"INDI": "NA", "NAME": "NA", "SEX": "NA", "BIRT": "NA", "DEAT": "NA", "FAMC": "NA", "FAMS": "NA"}
 
@@ -36,37 +52,33 @@ class Project3:
         for line in lines:
             words = line.strip().split()
             if words[0] == "0":
-                if words[-1] in indi_tab:
+                if words[-1] in indi_tab :
                     if indi_tab[words[-1]] == "NA":
                         indi_tab[words[-1]] = words[1]
-                    else:
-                        data = list(indi_tab.values())
-                        c.execute(
-                            "INSERT INTO indi (INDI, NAME, SEX, BIRT, DEAT, FAMC, FAMS) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                            (data[0], data[1], data[2], data[3], data[4], data[5], data[6]))
-                        conn.commit()
 
+                    if indi_tab[words[-1]] != "NA":
+                        self.insert_entry(indi_tab, c, conn)
                         for key in indi_tab:
                             indi_tab[key] = "NA"
 
                         indi_tab[words[-1]] = words[1]
+
+                    if indi_tab[words[-1]] == "FAM":
+                        self.insert_entry(indi_tab, c, conn)
+
                 if words[-1] in fam_tab:
                     if fam_tab[words[-1]] == "NA":
                         fam_tab[words[-1]] = words[1]
-                    else:
-                        data = list(fam_tab.values())
-                        if isinstance(data[5], list):
-                            c.execute("INSERT INTO fam (FAM, MARR, DIV, HUSB, WIFE, CHIL) VALUES (?, ?, ?, ?, ?, ?)",
-                                      (data[0], data[1], data[2], data[3], data[4], ' '.join(data[5])))
-                        else:
-                            c.execute("INSERT INTO fam (FAM, MARR, DIV, HUSB, WIFE, CHIL) VALUES (?, ?, ?, ?, ?, ?)",
-                                      (data[0], data[1], data[2], data[3], data[4], data[5]))
-                        conn.commit()
 
+                    if fam_tab[words[-1]] != "NA":
+                        self.insert_entry(fam_tab, c, conn)
                         for key in fam_tab:
                             fam_tab[key] = "NA"
 
                         fam_tab[words[-1]] = words[1]
+                    if fam_tab[words[-1]] == "TRLR":
+                        self.insert_entry(fam_tab, c, conn)
+
             elif words[0] == "1":
                 if words[1] in date_tags:
                     date_name_cache = words[1]
@@ -103,7 +115,7 @@ class Project3:
         c.execute('SELECT FAM, MARR, DIV, HUSB, WIFE, CHIL FROM fam')
         return c.fetchall()
 
-    def print_table(self, c):
+    def print_selected(self, c):
         # indi = PrettyTable(["ID", "Name", "Gender", "Birthday", "Age", "Alive", "Death", "Child", "Spouse"])
         # fam = PrettyTable(
         #     ["ID", "Married", "Divorced", "Husband ID", "Husband Name", "Wife ID", "Wife Name", "Children"])
@@ -126,8 +138,8 @@ def main():
     c = conn.cursor()
 
     demo.create_table(c)
-    demo.parse_lines(c, conn)
-    demo.print_table(c)
+    demo.populate_table(c, conn)
+    demo.print_selected(c)
 
     c.close()
     conn.close()
