5c7f291e021a699dc9eff157e76fda764d6f9065
ywCN <wangyouhao90@gmail.com>
2017-09-16 21:20:19 -0400

    refactored 95%

--- Project3.py
+++ Project3.py
@@ -3,7 +3,7 @@ import datetime
 from prettytable import PrettyTable
 
 
-class OpenSavePrint:
+class Project3:
     def open_file(self):
         while True:
             file_name = input('Enter the file name: ')  # MyFamily.ged
@@ -20,46 +20,51 @@ class OpenSavePrint:
         c = conn.cursor()
         c.execute("CREATE TABLE IF NOT EXISTS indi(INDI TEXT, NAME TEXT, SEX TEXT, BIRT TEXT, DEAT TEXT, FAMC TEXT, FAMS TEXT)")
         c.execute("CREATE TABLE IF NOT EXISTS indi_fam(INDI TEXT, FAM TEXT)")
-        c.execute("CREATE TABLE IF NOT EXISTS fam(FAM TEXT, MARR TEXT, HUSB TEXT, WIFE TEXT, CHIL TEXT, DIV TEXT)")
+        c.execute("CREATE TABLE IF NOT EXISTS fam(FAM TEXT, MARR TEXT, DIV TEXT, HUSB TEXT, WIFE TEXT, CHIL TEXT)")  # CHIL's type may be wrong
         c.close()
         conn.close()
     def parse_lines(self):
         conn = sqlite3.connect('project.db')
         c = conn.cursor()
 
+        indi_tab = {"INDI": "", "NAME": "", "SEX": "", "BIRT": "", "DEAT": "", "FAMC": "", "FAMS": ""}
+        fam_tab = {"FAM": "", "MARR": "", "DIV": "", "HUSB": "", "WIFE": "", "CHIL": []}
         date_tags = ["BIRT", "DEAT", "MARR", "DIV"]
-        lines = self.open_file()
-        date_name_cache = indi_id = fam_id = ""
-        indi_tab = {"INDI": "", "NAME": "", "SEX": "", "FAMC": "", "FAMS": "", "BIRT": "", "DEAT": ""}
-        fam_tab = {"FAM": "", "MARR": "", "HUSB": "", "WIFE": "", "CHIL": "", "DIV": ""}
+        date_name_cache = ""
 
+        lines = self.open_file()
         for line in lines:
             words = line.strip().split()
             if words[0] == "0":
-                if words[-1] == "INDI":
-                    if indi_id != "":
+                if words[-1] in indi_tab:
+                    if indi_tab[words[-1]] != "":
+                        data = list(fam_tab.values())
                         c.execute("INSERT INTO indi (INDI, NAME, SEX, BIRT, DEAT, FAMC, FAMS) VALUES (?, ?, ?, ?, ?, ?)",
-                                  (indi_tab["INDI"], indi_tab["NAME"], indi_tab["SEX"], indi_tab["FAMC"], indi_tab["FAMS"], indi_tab["BIRT"], indi_tab["DEAT"]))
+                                  (data[0], data[1], data[2], data[3], data[4], data[5], data[6]))
                         conn.commit()
 
                         for key in indi_tab.keys():
                             indi_tab[key] = ""
                     else:
                         indi_tab[words[-1]] = words[1]
-                if words[-1] == "FAM":
-                    if indi_id != "":
-                        c.execute("INSERT INTO fam (FAM, MARR, HUSB, WIFE, CHIL, DIV) VALUES (?, ?, ?, ?, ?)",
-                            (fam_tab["FAM"], fam_tab["MARR"], fam_tab["HUSB"], fam_tab["WIFE"], fam_tab["CHIL"], fam_tab["DIV"]))
+                if words[-1] in fam_tab:
+                    if fam_tab[words[-1]] != "":
+                        data = list(fam_tab.values())
+                        c.execute("INSERT INTO fam (FAM, MARR, DIV, HUSB, WIFE, CHIL) VALUES (?, ?, ?, ?, ?)",
+                            (data[0], data[1], data[2], data[3], data[4], data[5]))
                         conn.commit()
 
                         for key in fam_tab.keys():
-                            fam_tab[key] = ""
+                            if isinstance(key, list):
+                                fam_tab[key][:] = []
+                            else:
+                                fam_tab[key] = ""
                     else:
                         fam_tab[words[-1]] = words[1]
 
             elif words[0] == "1":
                 if words[1] in date_tags:
-                    date_name_cache = words[1]  # cache it for "DATE"
+                    date_name_cache = words[1]
                 elif words[1] in indi_tab:
                     indi_tab[words[1]] = " ".join(words[2:])
                 elif words[1] in fam_tab:
@@ -70,8 +75,10 @@ class OpenSavePrint:
             elif words[0] == "2" and words[0] == "DATE":
                 if date_name_cache in indi_tab:
                     indi_tab[date_name_cache] = " ".join(words[2:])
+                    date_name_cache = ""  # optional
                 elif date_name_cache in fam_tab:
                     fam_tab[date_name_cache] = " ".join(words[2:])
+                    date_name_cache = ""
                 else:
                     print("Something is wrong with the date_name_cache!")
         c.close()
@@ -81,18 +88,18 @@ class OpenSavePrint:
 
 
     def print_table(self):
-        individual = PrettyTable(["ID", "Name", "Gender", "Birthday", "Age", "Alive", "Death", "Child", "Spouse"])
-        family = PrettyTable(["ID", "Married", "Divorced", "Husband ID", "Husband Name", "Wife ID", "Wife Name", "Children"])
+        indi = PrettyTable(["ID", "Name", "Gender", "Birthday", "Age", "Alive", "Death", "Child", "Spouse"])
+        fam = PrettyTable(["ID", "Married", "Divorced", "Husband ID", "Husband Name", "Wife ID", "Wife Name", "Children"])
 
-        # TODO: Populate two tables by using data from two dictionaries
-        # Take care of "age","alive", "Husband Name","Wife Name"
+        # TODO: Populate two tables by using data from two tables
+        # Take care of "age","alive", "Husband Name","Wife Name" optional for project3?
 
 
 
 
 def main():
-    test = OpenSavePrint()
-    test.parse_lines()  # for testing
+    demo = Project3()  # for testing
+    demo.parse_lines()
 
 
 if __name__ == '__main__':
