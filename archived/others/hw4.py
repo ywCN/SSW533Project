997e05f4a7df224e54545747046cab7f2e40d22f
ywCN <wangyouhao90@gmail.com>
2017-09-25 22:52:45 -0400

    debugged hw4, added more test cases

--- archived/others/hw4.py
+++ archived/others/hw4.py
@@ -71,11 +71,17 @@ class HW4:
 class TestHW4(unittest.TestCase):
     def test_marriage_before_death(self):
         expected = True  # because my data is valid, invalid data won't pass test
+        unexpected = False  # This should not happen if the given data is valid.
+        self.assertTrue(HW4().marriage_before_death())
         self.assertTrue(HW4().marriage_before_death() == expected)
+        self.assertFalse(HW4().marriage_before_death() == unexpected)
 
     def test_divorce_before_death(self):
-        expected = True  # because my data is valid, invalid data won't pass test
-        self.assertTrue(HW4().marriage_before_death() == expected)
+        expected = True
+        unexpected = False
+        self.assertTrue(HW4().divorce_before_death())
+        self.assertTrue(HW4().divorce_before_death() == expected)
+        self.assertFalse(HW4().divorce_before_death() == unexpected)
 
     def test_query_info(self):
         test_query1 = "select INDI, NAME, DEAT, fam.MARR from indi INNER JOIN fam " \
@@ -83,7 +89,9 @@ class TestHW4(unittest.TestCase):
         test_query2 = "select INDI, NAME, DEAT, fam.DIV from indi INNER JOIN fam " \
                       "ON INDI.INDI = FAM.HUSB OR INDI.INDI = FAM.WIFE"
         self.assertEqual(len(HW4().query_info(test_query1)[0]), 4)
+        self.assertEqual(len(HW4().query_info(test_query1)), 8)
         self.assertEqual(len(HW4().query_info(test_query2)[0]), 4)
+        self.assertEqual(len(HW4().query_info(test_query2)), 8)
 
 
 def main():
