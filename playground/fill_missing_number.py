725bae9e38c628bd4a3f38e77f1081c229ce3c47
ywCN <wangyouhao90@gmail.com>
2017-09-29 15:50:54 -0400

    debugged parsing

--- playground/fill_missing_number.py
+++ playground/fill_missing_number.py
@@ -3,20 +3,16 @@ numbers = f.split(',')
 res = []
 
 q = []
-print(type(q))
-print(type(numbers[0]))
-print(type(int(numbers[0])))
 for number in numbers:
     q.append(int(number))
 
-print(len(q))
 counter = 1
-while counter < 600:
+while counter < 580:
     if counter in q:
         q.remove(counter)
         res.append(counter)
     else:
-        counter += 1
         res.append('XXX')
-
-print(res)
+    counter += 1
+print(len(res))
+print(res[560:])
