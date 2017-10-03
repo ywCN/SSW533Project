b7bb45fdaeb05d9c889a6f86406ca6c2f854d597
ywCN <wangyouhao90@gmail.com>
2017-10-03 09:56:08 -0400

    removed return true for always returns true

--- Project4.py
+++ Project4.py
@@ -26,7 +26,7 @@ class HW4:
     def dates_before_current_date(self):
         """
         US01 - Dates before current date
-        :rtype: bool
+        :rtype: None
         """
         query = "select INDI, NAME, BIRT, DEAT, fam.MARR, fam.DIV from indi LEFT JOIN fam ON INDI.INDI = FAM.HUSB OR " \
                 "INDI.INDI = FAM.WIFE "
@@ -50,13 +50,11 @@ class HW4:
             for date in dates:
                 if date != "NA" and date > today:
                     print("ERROR: US01: {} occurs after today {} for {}".format(date, today, row[0] + row[1]))
-                    # return False
-        return True
 
     def birth_before_marriage(self):
         """
         US02 - Birth before marriage
-        :rtype: bool
+        :rtype: None
         """
         query = "select INDI, NAME, BIRT, fam.MARR from indi INNER JOIN fam ON INDI.INDI = FAM.HUSB OR INDI.INDI = " \
                 "FAM.WIFE "
@@ -66,13 +64,11 @@ class HW4:
                 marriage = datetime.strptime(row[3], '%Y-%m-%d').date()
                 if birth > marriage:
                     print("ERROR: US02: Birth {} occurs after marriage {} for {}".format(birth, marriage, row[0] + row[1]))
-                    # return False
-        return True
 
     def birth_before_death(self):
         """
         US03 - Birth before death
-        :rtype: bool
+        :rtype: None
         """
         query = "select INDI, NAME, BIRT, DEAT from indi"
         for row in self.query_info(query):
@@ -81,13 +77,11 @@ class HW4:
                 death = datetime.strptime(row[3], '%Y-%m-%d').date()
                 if birth > death:
                     print("ERROR: US03: Birth {} occurs after death {} for {}".format(birth, death, row[0] + row[1]))
-                    # return False
-        return True
 
     def marriage_before_divorce(self):
         """
         US04 - Marriage before divorce
-        :rtype: bool
+        :rtype: None
         """
         query = "select INDI, NAME, fam.MARR, fam.DIV from indi INNER JOIN fam " \
                 "ON INDI.INDI = FAM.HUSB OR INDI.INDI = FAM.WIFE"
@@ -97,13 +91,11 @@ class HW4:
                 divorce = datetime.strptime(row[3], '%Y-%m-%d').date()
                 if marry > divorce:
                     print("ERROR: US04: Marriage {} occurs after divorce {} for {}".format(marry, divorce, row[0] + row[1]))
-                    # return False
-        return True
 
     def marriage_before_death(self):
         """
         US05 - Marriage before death
-        :rtype: bool
+        :rtype: None
         """
         query = "select INDI, NAME, DEAT, fam.MARR from indi INNER JOIN fam " \
                 "ON INDI.INDI = FAM.HUSB OR INDI.INDI = FAM.WIFE"
@@ -114,13 +106,11 @@ class HW4:
                 marry = datetime.strptime(row[3], '%Y-%m-%d').date()
                 if marry > death:  # cannot marry after death
                     print("ERROR: US05: Marriage {} occurs after death {} for {}".format(marry, death, row[0] + row[1]))
-                    # return False
-        return True
 
     def divorce_before_death(self):
         """
         US06 - Divorce before death
-        :rtype: bool
+        :rtype: None
         """
         query = "select INDI, NAME, DEAT, fam.DIV from indi INNER JOIN fam " \
                 "ON INDI.INDI = FAM.HUSB OR INDI.INDI = FAM.WIFE"
@@ -131,13 +121,11 @@ class HW4:
                 divorce = datetime.strptime(row[3], '%Y-%m-%d').date()
                 if divorce > death:  # cannot divorce after death
                     print("ERROR: US06: Divorce {} occurs after death {} for {}".format(divorce, death, row[0] + row[1]))
-                    # return False
-        return True
 
     def less_than_150_years_old(self):
         """
         US07 - Less then 150 years old
-        :rtype: bool
+        :rtype: None
         """
         query = "select INDI, NAME, BIRT, DEAT from indi"
         today = datetime.today().date()
@@ -151,14 +139,19 @@ class HW4:
                 age = today.year - birth.year
             if age >= 150:
                 print("ERROR: US07: Age is greater than or equal to 150 years for {}".format(row[0] + row[1]))
-                # return False
-        return True
+
 
     def birth_before_marriage_of_parents(self):
         """
         US08 - Less then 150 years old
         :rtype: bool
         """
+        query1 = "select INDI from indi WHERE FAMC NOT LIKE 'NA'"
+        children = self.query_info(query1)  # ids
+        query2 = "select MARR, DIV, CHIL from fam"
+        dates = self.query_info(query2)  # marr div chil  type: list[tuple(str)]
+
+
 
     def query_info(self, query):
         """
@@ -187,6 +180,7 @@ class HW4:
         self.marriage_before_death()
         self.divorce_before_death()
         self.less_than_150_years_old()
+        self.birth_before_marriage_of_parents()
 
 
 
